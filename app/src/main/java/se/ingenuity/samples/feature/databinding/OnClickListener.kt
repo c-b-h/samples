36cda44b225b27da5129b07c50fe3ee9a990ac21
Cyrus Bakhtiari-Haftlang <cyrus.bh@gmail.com>
2020-09-29 21:19:09 +0200

    Bug related to TextAppearanceSpan and TypefaceSpan

--- app/src/main/java/se/ingenuity/samples/feature/databinding/OnClickListener.kt
+++
@@ -1,5 +0,0 @@
-package se.ingenuity.samples.feature.databinding
-
-fun interface OnClickListener<I> {
-    fun onClick(item: I)
-}
--- app/src/main/java/se/ingenuity/samples/feature/main/MainActivity.kt
+++ app/src/main/java/se/ingenuity/samples/feature/main/MainActivity.kt
@@ -1,20 +1,49 @@
 package se.ingenuity.samples.feature.main
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import android.text.TextUtils
+import android.text.style.TextAppearanceSpan
+import android.widget.TextView
+import androidx.core.text.set
+import androidx.core.text.toSpannable
 import se.ingenuity.samples.R
-import se.ingenuity.samples.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity__main
-        )
+        setContentView(R.layout.activity__main)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        val textView = findViewById<TextView>(R.id.spanned_text)
+
+        val spannableText = textView.text.toSpannable()
+
+        var start = TextUtils.indexOf(textView.text, "regular")
+        var end = start + "regular".length
+        spannableText[start, end] =
+            TextAppearanceSpan(this, R.style.TextAppearance_AppTheme_Regular)
+
+        start = TextUtils.indexOf(textView.text, "bold")
+        end = start + "bold".length
+        spannableText[start, end] =
+            TextAppearanceSpan(this, R.style.TextAppearance_AppTheme_Bold)
+
+        start = TextUtils.indexOf(textView.text, "italic")
+        end = start + "italic".length
+        spannableText[start, end] =
+            TextAppearanceSpan(this, R.style.TextAppearance_AppTheme_Italic)
+
+        start = TextUtils.indexOf(textView.text, "bold italic")
+        end = start + "bold italic".length
+        spannableText[start, end] =
+            TextAppearanceSpan(this, R.style.TextAppearance_AppTheme_Bold)
+        spannableText[start, end] =
+            TextAppearanceSpan(this, R.style.TextAppearance_AppTheme_Italic)
+        textView.text = spannableText
     }
 }
